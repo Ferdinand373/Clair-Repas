4ce0b228ff19cd0af24c906c2da746aa345bc56c
Ferdinand373 <freddycogrel@live.com>
2026-08-08 09:17:21 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='clair-repas-v2.2-moteur-cuisine';
+const CACHE='clair-repas-v2.3-planning-minuteurs-etapes';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
