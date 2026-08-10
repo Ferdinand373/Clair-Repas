8182742c9361613be32f6838a0628b27f72f840d
Ferdinand373 <freddycogrel@live.com>
2026-08-10 21:51:02 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='clair-repas-v36-planification-evidente-20260810';
+const CACHE='clair-repas-v36-planification-fix-20260810';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
