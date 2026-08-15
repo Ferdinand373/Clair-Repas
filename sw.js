a31afc86cabdf520cf92c360ebd0f31449eb1de9
Ferdinand373 <freddycogrel@live.com>
2026-08-15 15:32:55 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='clair-repas-v43-courses-controlees-20260814';
+const CACHE='clair-repas-v431-poissons-20260815';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
