c790e485980551cb86cf58286ee03fd1e91c38a8
Ferdinand373 <freddycogrel@live.com>
2026-08-15 16:22:18 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='clair-repas-v45-variete-multijours-20260815';
+const CACHE='clair-repas-v46-contexte-repas-20260815';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
