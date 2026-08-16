8fa63599567d1cebd297fce714b47bb389c9592c
Ferdinand373 <freddycogrel@live.com>
2026-08-16 19:34:04 +0200

    Clair Repas V7.5 — Grands Chefs

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 "use strict";
-const CACHE='clair-repas-v74-tables-de-france-20260816';
+const CACHE='clair-repas-v75-grands-chefs-20260816';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
 async function cacheCore(){
